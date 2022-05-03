336f91cb2a70238040d94b61f7afa01daa8d2419
oscar baquero <osbbarr1@gmail.com>
2022-05-03 05:08:01 -0500

    Inicio proyecto

---
+++ src/test/java/com/grupollano/SuperinventApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupollano;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperinventApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
